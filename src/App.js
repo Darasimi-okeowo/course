bbc0071495c4bd6d9de9e0224a317ab4ccd41db4
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-09-06 14:51:44 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+import Expenses from './Component/Expenses/Expenses';
+import NewExpense from './Component/NewExpense/NewExpense';
 import "./App.css";
 
 function App() {
-  // const [count,setCount] = useState(0);
-  // const [toggle, setToggle] = useState(false)
-  const [data, setData] = useState({
-    names: "",
-    classNames: "",
-    surname: "",
-  });
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setData((props) => ({
-      ...props,
-      [name]: value,
-    }));
-    console.log(data);
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
   };
 
-  const handleClick = (e) => {
-    // setCount((prev) => prev + 1)
-    // setToggle(!toggle)
-    e.preventDefault();
-  console.log(data);
-    setData({
-      names: "",
-      classNames: "",
-      surname: ""
-    });
-  };
   return (
-    <div className="App">
-      {/* <p>{count}</p> */}
-      {/* {toggle ? <h1>helllo</h1> : 'hhhgfhh'} */}
-      <form onSubmit={handleClick}>
-        <input
-          placeholder="name"
-          name="names"
-          value={data.names}
-          onChange={handleChange}
-        />
-        <input
-          placeholder="class-name"
-          name="classNames"
-          value={data.classNames}
-          onChange={handleChange}
-        />
-        <input
-          placeholder="surname"
-          name="surname"
-          value={data.surname}
-          onChange={handleChange}
-        />
-        <button>submit</button>
-      </form>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
